06a082c62ce1ba3eab8da962b7c10fb6ac23b7ae
Farida <farida.alin@yahoo.com>
2020-09-02 10:21:05 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    borderRadius: {
+      'lg': '1em',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
